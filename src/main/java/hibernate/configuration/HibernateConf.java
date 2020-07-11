f350c954171b14b798fe227104031bd40e5a9af5
Bizon15100 <rafal15100@gmail.com>
2020-07-11 11:43:58 +0200

    Fixed Bugs

--- src/main/java/hibernate/configuration/HibernateConf.java
+++ src/main/java/hibernate/configuration/HibernateConf.java
@@ -1,17 +1,23 @@
 package hibernate.configuration;
 
+import hibernate.entity.Adress;
+import hibernate.entity.Phone;
+import hibernate.entity.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateConf {
 
-    private static final String CONFIG_FILE = "hibernate.cfg.xml";
+
     private static SessionFactory sessionFactory;
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
-            Configuration configuration = new Configuration().configure(CONFIG_FILE);
+            Configuration configuration = new Configuration();
             sessionFactory = configuration.buildSessionFactory();
+            configuration.addAnnotatedClass(Adress.class);
+            configuration.addAnnotatedClass(Phone.class);
+            configuration.addAnnotatedClass(User.class);
         }
         return sessionFactory;
     }
